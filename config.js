0e0321aa6f4a14f80dc97d1ba5b9ce7c9583fdee
wenting0429 <108654992+wenting0429@users.noreply.github.com>
2026-08-28 01:18:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // The Supabase Publishable key is intended for browser/front-end use.
 // NEVER put a Supabase secret key or service_role key here.
 window.HYROX_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
-  SUPABASE_PUBLISHABLE_KEY: "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE"
+  SUPABASE_URL: "https://jnjsamofsyunjkczjajq.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_nVUf6iCbs2xE_hgQuImUpg_prV83SI7"
 };
